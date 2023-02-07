bd19fc8a0d71a4537720e0f328e7eaf8abe9b87a
AICaponi <gustavocaponi@hotmail.com>
2023-02-07 16:54:37 -0300

    mudanças 07-02-2023

--- src/main/java/com/gustavocaponi/helpdesk/domain/enums/Status.java
+++ src/main/java/com/gustavocaponi/helpdesk/domain/enums/Status.java
@@ -2,7 +2,7 @@ package com.gustavocaponi.helpdesk.domain.enums;
 
 public enum Status {
 
-	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"),PENDENTE(3,"PENDENTE"), ENCERRADO(2, "ENCERRADO");
 	
 	private Integer codigo;
 	private String descricao;
